a85323a25360143c7285763c6b1be7ae0c063670
Arnav7107 <arnavverma345@gmail.com>
2024-08-25 11:50:35 +0530

    alow CORS

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
-app.use(express.json()); 
+app.use(cors());
+
+app.use(express.json());
 
 app.get('/bfhl', (req, res) => {
     res.status(200).json({
